68d6150757d270414a30bed7b5131eddf598ff50
elopez-gar <emmanuellopez121100@gmail.com>
2022-06-07 17:35:53 -0500

    Laboratorio 2.1.1.18 - Emmanuel

---
+++ basico/modulo2/lab21118.py
@@ -0,0 +1,7 @@
+#Laboratorio 2.1.1.18
+#Elaborado por: Emmanuel López García
+print("Fundamentos","Programación","en")
+print("Python")
+print()
+print("Fundamentos","Programación","en", sep="_", end=" *")
+print("Python")
