9cc61b207bfeefeeb1189637db6a00625c235a0f
elopez-gar <emmanuellopez121100@gmail.com>
2022-06-07 17:53:04 -0500

    Laboratorio 2.1.1.19 sin finalizar

---
+++ basico/modulo2/lab21119.py
@@ -0,0 +1,4 @@
+#Laboratorio 2.1.1.19
+#Elaborado por: Emmanuel López García
+print("    *\n" , "   * *\n", "  *   *\n", " *     *\n", sep="", end="")
+print("***   ***\n", "  *   *\n", "  *   *\n", "  *****\n", sep="")
