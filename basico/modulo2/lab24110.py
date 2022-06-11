3cc581d74320a850120e74775ce325e1004bda66
elopez-gar <emmanuellopez121100@gmail.com>
2022-06-10 20:41:07 -0500

    Todo el avance del día 10 de junio

---
+++ basico/modulo2/lab24110.py
@@ -0,0 +1,9 @@
+#Laboratorio 2.4.1.10
+#Elaborado por: Emmanuel López García
+#Fecha: 10 de junio del 2022
+
+x=input("Ingresa el valor de x: ")
+x = float(x)
+y = (3*x**3)-(2*x**2)+(3*x)-1
+# escribe tu código aquí
+print("y =", y)
---
+++ basico/modulo2/lab2512.py
@@ -0,0 +1,16 @@
+#Laboratorio 2.5.1.2
+#Elaborado por: Emmanuel López García
+#Fecha: 10 de junio del 2022
+
+horas = 2 # número de horas
+segundos = 3600 # número de segundos en una hora
+
+print("Horas: ", horas) #imprime el numero de horas
+print("Segundos en Horas: ", horas * segundos) # se imprime el numero de segundos en determinado numero de horas
+
+'''
+aquí también se debe de imprimir un "Adiós", 
+pero el programador no tuvo tiempo de escribirlo.
+Este el es fin del programa que calcula el numero 
+de segundos en 2 horas
+'''
