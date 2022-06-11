b7a226ad81a53e82771a053d59dfb2e71993220c
Emmanuel Lopez Garcia <emmanuel@emmanuel.linux.mint>
2022-06-11 18:03:07 -0500

    Laboratorio 2.1.1.19 corregido y finalizado

--- basico/modulo2/lab21119.py
+++ basico/modulo2/lab21119.py
@@ -1,21 +1,40 @@
 #Laboratorio 2.1.1.19
 #Elaborado por: Emmanuel López García
 
+'''
 print("REDUCIENDO EL CÓDIGO")
 print("    *\n" , "   * *\n", "  *   *\n", " *     *\n", sep="", end="")
 print("***   ***\n", "  *   *\n", "  *   *\n", "  *****\n", sep="")
 print()
+
 print("AUMENTANDO EL TAMAÑO")
+print("    *\n"*2 , "   * *\n"*2, "  *   *\n"*2, " *     *\n"*2, sep="", end="")
+print("***   ***\n"*2, "  *   *\n"*2, "  *   *\n"*2, "  *****\n"*2, sep="")
+
 
 print()
 print("DUPLICANDO FLECHAS")
-print("    *"*2)
-print("   * *"*2)
-print("  *   *"*2)
-print(" *     *"*2)
-print("***   ***"*2)
-print("  *   *"*2)
-print("  *   *"*2)
-print("  *****"*2)
+print("AUMENTANDO EL TAMAÑO")
+print("    *\n"*2 , "   * *\n"*2, "  *   *\n"*2, " *     *\n"*2, sep="", end="")
+print("***   ***\n"*2, "  *   *\n"*2, "  *   *\n"*2, "  *****\n"*2, sep="\n")
 
+
+'''
+print("REDUCIENDO EL CÓDIGO")
+print("    *\n","   * *\n","  *   *\n"," *     *\n","***   ***\n","  *   *\n","  *   *\n",
+"  *****\n",sep="")
+print()
+
+print("AUMENTANDO EL TAMAÑO")
+print("        *\n","       * *\n","      *   *\n","     *     *\n","    *       *\n",
+    "   *         *\n","  *           *\n"," *             *\n","******     ******\n",
+    "     *     *\n","     *     *\n","     *     *\n","     *     *\n","     *******\n",sep="")
+print()
+
+print("DUPLICANDO FLECHAS")
+print(
+    "        *         "*2,"       * *        "*2,"      *   *       "*2,"     *     *      "*2,
+    "    *       *     "*2,"   *         *    "*2,"  *           *   "*2," *             *  "*2,
+    "******     ****** "*2,"     *     *      "*2,"     *     *      "*2,"     *     *      "*2,
+    "     *     *      "*2,"     *******      "*2,sep="\n")
 
