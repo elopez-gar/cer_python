e5c05116136bdcc2f86e029450132b659f3a7067
elopez-gar <emmanuellopez121100@gmail.com>
2022-06-10 20:14:20 -0500

    Avance hasta lab2.4.1.7

--- basico/modulo2/lab21119.py
+++ basico/modulo2/lab21119.py
@@ -1,4 +1,21 @@
 #Laboratorio 2.1.1.19
 #Elaborado por: Emmanuel López García
+
+print("REDUCIENDO EL CÓDIGO")
 print("    *\n" , "   * *\n", "  *   *\n", " *     *\n", sep="", end="")
 print("***   ***\n", "  *   *\n", "  *   *\n", "  *****\n", sep="")
+print()
+print("AUMENTANDO EL TAMAÑO")
+
+print()
+print("DUPLICANDO FLECHAS")
+print("    *"*2)
+print("   * *"*2)
+print("  *   *"*2)
+print(" *     *"*2)
+print("***   ***"*2)
+print("  *   *"*2)
+print("  *   *"*2)
+print("  *****"*2)
+
+
---
+++ basico/modulo2/lab22111.py
@@ -0,0 +1,4 @@
+#Laboratorio 2.2.1.11
+#Elaborado por: Emmanuel López García
+#Fecha: 10 de junio del 2022
+print("\"Estoy\"","\"aprendiendo\"","\"Python\"", sep="\n")
