684a3a1cbb1da955437c44ac922947e8dd3e14b6
Emmanuel Lopez Garcia <emmanuel@emmanuel.linux.mint>
2022-06-15 10:53:56 -0500

    Todo listo hasta el laboratorio 3.1.1.10

--- basico/modulo3/lab32110.py
+++ basico/modulo3/lab32110.py
@@ -1,6 +1,6 @@
 #Laboratorio 3.2.1.10
 #Elaborado por: Emmanuel López García
-#Fecha: 14 de junio del 2022
+#Fecha: 15 de junio del 2022
 
 # Indicar al usuario que ingrese una palabra
 # y asignarlo a la variable user_word.
@@ -10,6 +10,23 @@ user_word = user_word.upper()
 #print(user_word)
 
 for letter in user_word:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+            
+            print(letter, "\n")
+    
+print ("El ciclo ha sido finalizado")
+    
+
 
 
 #for letter in user_word:
