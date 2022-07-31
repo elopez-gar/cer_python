0ec4d5990dc473150f7b7be3fc38bd95f8771557
Emmanuel Lopez Garcia <emmanuel@emmanuel.linux.mint>
2022-07-31 13:20:54 -0500

    Lab 2.8.14 finalizado

---
+++ intermedio/modulo2/lab2814.py
@@ -0,0 +1,22 @@
+#Laboratorio 2.4.1.6
+#Elaborado por: Emmanuel López García
+#Fecha: 31 de julio del 2022
+
+def read_int(prompt, min, max):
+    bien = False
+    while not bien:
+        try:
+            valor = int(input(prompt))
+            bien = True
+        except ValueError:
+            print("Error: entrada incorrecta")
+        if bien:
+            bien = valor >= min and valor <= max
+        if not bien:
+            print("Error: el valor no esta dentro del rango permitido (" + str(min) + ".." + str(max) + ")")
+    return valor;
+
+
+v = read_int("Ingresa un número entre -10 a 10: ", -10, 10)
+
+print("El número es:", v)
